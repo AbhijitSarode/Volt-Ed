e4a3fe94eb8896e4475466bc5fe8556ff42829fd
Abhijit Sarode <arsarode@hotmail.com>
2024-02-25 21:34:55 +0530

    #34 signup function complete

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -36,7 +36,6 @@ const { passwordUpdatedEmail } = require("../mails/passwordUpdatedEmail");
  * @param {Object} res - The response object for sending the OTP delivery status and the generated OTP.
  * @returns {Object} - Returns a response indicating the success or failure of the OTP delivery process.
  */
-
 exports.sendOTP = async (req, res) => {
   try {
     // Fetch email from request body
@@ -91,6 +90,141 @@ exports.sendOTP = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Unable to send OTP",
+      error: error.message,
+    });
+  }
+};
+
+/**
+ * #### User Registration and OTP Verification
+ * **Functionality:**
+ * - Handles user registration and OTP verification.
+ *
+ * **Input:**
+ * - Expects user details and OTP in the request body.
+ *
+ * **Validation:**
+ * - Checks mandatory fields presence, password correctness and OTP uniqueness.
+ *
+ * **Password Handling:**
+ * - Encrypts the password before storage.
+ *
+ * **User Data Storage:**
+ * - Saves user, profile data and generates a profile image.
+ *
+ * **Output:**
+ * - Returns success status and user data upon successful registration.
+ *
+ * **Error Handling:**
+ * - Manages server issues, existing user detection, invalid OTP, and password mismatch scenarios.
+ *
+ * @param {Object} req - The request object containing user details and OTP.
+ * @param {Object} res - The response object for sending registration status and user data.
+ * @returns {Object} - Returns a response indicating the success or failure of the registration process.
+ */
+exports.signup = async (req, res) => {
+  try {
+    // Fetch data from request body
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      confirmPassword,
+      accountType,
+      otp,
+    } = req.body;
+
+    // Check if required fields are not empty
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      !password ||
+      !confirmPassword ||
+      !accountType ||
+      !otp
+    ) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    // Check if the password and confirm password match
+    if (password !== confirmPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Passwords do not match",
+      });
+    }
+
+    // Check if user is already registered
+    const userPresent = await User.findOne({ email });
+
+    // If user is already registered, let client know
+    if (userPresent) {
+      return res.status(400).json({
+        success: false,
+        message: "User already registered",
+      });
+    }
+
+    // Find the recent OTP in the database
+    const otpPresent = await OTP.findOne({ email })
+      .sort({ createdAt: -1 })
+      .limit(1);
+    console.info(`Recent OTP is: ${otpPresent}`);
+
+    // Validate the OTP
+    if (otpPresent.length == 0) {
+      return res.status(400).json({
+        success: false,
+        message: "OTP Expired",
+      });
+    } else if (otpPresent[0].otp !== otp) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid OTP",
+      });
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create entry in the Profile collection
+    const profileDetails = await Profile.create({
+      gender: null,
+      dateOfBirth: null,
+      about: null,
+      contact: null,
+    });
+
+    // Create a new user
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+      accountType,
+      additionalDetails: profileDetails._id,
+      image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName}%20${lastName}`,
+    });
+
+    // Send welcome email to the user
+    mailSender(email, "Welcome to the platform", "Welcome to the platform");
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: { user, profileDetails },
+    });
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Unable to register user",
       error: error.message,
     });
   }
