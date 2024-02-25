6b4a483b1fccdce5e8d60654952ffd58f37c02d8
Abhijit Sarode <arsarode@hotmail.com>
2024-02-25 19:23:15 +0530

    Setup complete

---
+++ server/server.js
@@ -0,0 +1,24 @@
+// Import dependencies
+const express = require("express"); // Import express framework
+const dotenv = require("dotenv").config(); // Load env variables
+const cors = require("cors"); // Library to enable CORS
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middlewares
+app.use(express.json()); // Parse incoming json request
+app.use(cors({ origin: "http://localhost:5173", credentials: true })); // Enables cors for requests from localhost
+
+// Home Route
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: "Server is up and running....",
+  });
+});
+
+// Start Server
+app.listen(PORT, () => {
+  console.info(`Server started on port ${PORT}`);
+});
