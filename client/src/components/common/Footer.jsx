0f0205668cd0d1496320ff270d4aff2f45b3da20
Abhijit Sarode <arsarode@hotmail.com>
2024-02-25 23:39:39 +0530

    Common Component - Footer

---
+++ client/src/components/common/Footer.jsx
@@ -0,0 +1,141 @@
+// Import Dependencies
+import { Link } from "react-router-dom";
+import { FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
+
+// Import Files
+import { FooterLink2 } from "../../data/footer-links";
+import Logo from "../../assets/Logo/Logo_Full.svg";
+
+const BottomFooter = ["Privacy Policy", "Cookie Policy", "Terms"];
+const Resources = ["Articles", "Blog", "Chart Sheet", "Code challenges", "Docs", "Projects", "Videos", "Workspaces"];
+const Plans = ["Paid memberships", "For students", "Business solutions"];
+const Community = ["Forums", "Chapters", "Events"];
+
+const Footer = () => {
+    return (
+        <footer className="bg-richblack-800">
+            <div className="flex lg:flex-row gap-8 items-center justify-between w-11/12 max-w-maxContent text-richblack-400 leading-6 mx-auto relative py-14">
+                <div className="border-b w-[100%] flex flex-col lg:flex-row pb-5 border-richblack-700">
+
+                    {/* Section 1 */}
+
+                    <section className="lg:w-[50%] flex flex-wrap flex-row justify-between lg:border-r lg:border-richblack-700 pl-3 lg:pr-5 gap-3">
+                        <div className="w-[30%] flex flex-col gap-3 lg:w-[30%] mb-7 lg:pl-0">
+
+                            <img src={Logo} alt="volted logo" className="object-contain" />
+                            <h1 className="text-richblack-50 font-semibold text-[16px]">Company</h1>
+
+                            <div className="flex flex-col gap-2">
+                                {["About", "Careers", "Affiliates"].map((ele, i) => {
+                                    return (
+                                        <div key={i} className="text-[14px] cursor-pointer hover:text-richblack-50 transition-all duration-200">
+                                            <Link to={ele.toLowerCase()}>{ele}</Link>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                            <div className="flex gap-3 text-lg">
+                                <a href="https://www.linkedin.com/in/volted-academy/"><FaLinkedin className="hover:fill-[#0A66C2]" /></a>
+                                <a href="https://twitter.com/volted_academy"><FaTwitter className="hover:fill-[#1D9BF0]" /></a>
+                                <a href="https://www.youtube.com/@volted_academy"><FaYoutube className="hover:fill-[#FF0000]" /></a>
+                            </div>
+                        </div>
+
+                        <div className="w-[48%] lg:w-[30%] mb-7 lg:pl-0">
+                            <h1 className="text-richblack-50 font-semibold text-[16px]">Resources</h1>
+
+                            <div className="flex flex-col gap-2 mt-2">
+                                {Resources.map((ele, index) => {
+                                    return (
+                                        <div key={index} className="text-[14px] cursor-pointer hover:text-richblack-50 transition-all duration-200">
+                                            <Link to={ele.split(" ").join("-").toLowerCase()}>{ele}</Link>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+
+                            <h1 className="text-richblack-50 font-semibold text-[16px] mt-7">Support</h1>
+                            <div className="text-[14px] cursor-pointer hover:text-richblack-50 transition-all duration-200 mt-2">
+                                <Link to={"/help-center"}>Help Center</Link>
+                            </div>
+                        </div>
+
+                        <div className="w-[48%] lg:w-[30%] mb-7 lg:pl-0">
+                            <h1 className="text-richblack-50 font-semibold text-[16px]">Plans</h1>
+
+                            <div className="flex flex-col gap-2 mt-2">
+                                {Plans.map((ele, index) => {
+                                    return (
+                                        <div key={index} className="text-[14px] cursor-pointer hover:text-richblack-50 transition-all duration-200">
+                                            <Link to={ele.split(" ").join("-").toLowerCase()}>{ele}</Link>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+
+                            <h1 className="text-richblack-50 font-semibold text-[16px] mt-7">Community</h1>
+
+                            <div className="flex flex-col gap-2 mt-2">
+                                {Community.map((ele, index) => {
+                                    return (
+                                        <div key={index} className="text-[14px] cursor-pointer hover:text-richblack-50 transition-all duration-200">
+                                            <Link to={ele.split(" ").join("-").toLowerCase()}>{ele}</Link>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    </section>
+
+                    {/* Section 2 */}
+
+                    <section className="lg:w-[50%] flex flex-wrap flex-row justify-between pl-3 lg:pl-5 gap-3">
+                        {FooterLink2.map((ele, i) => {
+                            return (
+                                <div key={i} className="w-[48%] lg:w-[30%] mb-7 lg:pl-0">
+                                    <h1 className="text-richblack-50 font-semibold text-[16px]">{ele.title}</h1>
+
+                                    <div className="flex flex-col gap-2 mt-2">
+                                        {ele.links.map((link, index) => {
+                                            return (
+                                                <div key={index} className="text-[14px] cursor-pointer hover:text-richblack-50 transition-all duration-200" >
+                                                    <Link to={link.link}>{link.title}</Link>
+                                                </div>
+                                            );
+                                        })}
+                                    </div>
+
+                                </div>
+                            );
+                        })}
+                    </section>
+
+                </div>
+            </div>
+
+            <div className="flex flex-row items-center justify-between w-11/12 max-w-maxContent text-richblack-400 mx-auto  pb-14 text-sm">
+
+                {/* Footnote */}
+
+                <section className="flex justify-between lg:items-start items-center flex-col lg:flex-row gap-3 w-full">
+                    <div className="flex flex-row">
+                        {BottomFooter.map((ele, i) => {
+                            return (
+                                <div key={i}
+                                    className={` ${BottomFooter.length - 1 === i
+                                        ? ""
+                                        : "border-r border-richblack-700 cursor-pointer hover:text-richblack-50 transition-all duration-200"} px-3 `}>
+                                    <Link to={ele.split(" ").join("-").toLocaleLowerCase()}>{ele}</Link>
+                                </div>
+                            );
+                        })}
+                    </div>
+
+                    <div className="text-center">Made with ❤️ by <a href="https://www.youtube.com/@codeSculptors" className="hover:underline hover:text-amaranth-500 focus:text-amaranth-500 focus:underline text-white">Code Sculptors </a> © 2023 Volted</div>
+                </section>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer
