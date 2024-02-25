f63a0036f1c2d5237fdd817d4bda111a039e6a0d
Abhijit Sarode <arsarode@hotmail.com>
2024-02-25 23:13:58 +0530

    #38 delete section function complete

--- server/controllers/Section.js
+++ server/controllers/Section.js
@@ -62,3 +62,50 @@ exports.createSection = async (req, res) => {
     });
   }
 };
+
+/**
+ * #### Delete a section from a course
+ *
+ * - Expects: sectionId in req params.
+ * - Finds: the section in the Section database.
+ * - Deletes: the section from the Section database.
+ * - Returns: success status.
+ *
+ * @param {Object} req - The request object containing sectionId in params.
+ * @param {Object} res - The response object to send the success status.
+ * @returns {Object} - Returns a response containing the success status.
+ */
+exports.deleteSection = async (req, res) => {
+  try {
+    // Fetch the sectionId from the request params
+    const { sectionId } = req.params;
+
+    // Find section in database
+    const section = await Section.findById(sectionId);
+
+    // Check if section exists
+    if (!section) {
+      return res.status(404).json({
+        success: false,
+        message: "Section not found.",
+      });
+    }
+
+    // Delete the section
+    await Section.deleteOne(section); // May give error
+
+    // Send the success status
+    return res.status(200).json({
+      success: true,
+      message: "Section deleted successfully.",
+    });
+  } catch (error) {
+    // Send the error message
+    console.error(error);
+    res.status(500).json({
+      success: false,
+      message: "Unable to delete section.",
+      error: error.message,
+    });
+  }
+};
