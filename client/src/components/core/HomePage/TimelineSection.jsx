3972d5400ba8aab25ce40f52bf42cae409228450
Abhijit Sarode <arsarode@hotmail.com>
2024-03-10 13:56:39 +0530

    #67 Timeline section complete

---
+++ client/src/components/core/HomePage/TimelineSection.jsx
@@ -0,0 +1,30 @@
+import HighlightText from "../../common/HighlightText";
+import Timeline from "./Timeline";
+
+const TimelineSection = () => {
+  return (
+    <div className=" text-richblack-700">
+      <div className="mx-auto flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 ">
+        <div className="mb-10 mt-[-100px] flex flex-col justify-between gap-10 lg:mt-20 lg:flex-row lg:gap-0">
+          <h2 className="text-4xl font-semibold lg:w-[45%] ">
+            Get the skills you need for a
+            <HighlightText
+              text={" job that is in demand."}
+              gradient={"custom-gradient2"}
+            />
+          </h2>
+          <div className="flex flex-col items-start gap-10 lg:w-[40%]">
+            <p className="text-[16px]">
+              The modern Volt-Ed dictates its own terms. Today, to be a
+              competitive specialist requires more than professional skills.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <Timeline />
+    </div>
+  );
+};
+
+export default TimelineSection;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@
 import HeroSection from "../components/core/HomePage/HeroSection";
 import CodeSection from "../components/core/HomePage/CodeSection";
 import ExploreMoreSection from "../components/core/HomePage/ExploreMoreSection";
+import TimelineSection from "../components/core/HomePage/TimelineSection";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <HeroSection />
       <CodeSection />
       <ExploreMoreSection />
+      <TimelineSection />
     </>
   );
 };
